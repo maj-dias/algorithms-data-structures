746401daa7334297c336812acdeff89aec2bfc1b
Matheus <matheus21st@gmail.com>
2023-09-17 20:40:37 -0300

    create max_heap.py

---
+++ max_heap.py
@@ -0,0 +1,21 @@
+# =================================================================================================
+# Python Implementation of Max Heap
+# Author: Matheus José Oliveira dos Santos
+# =================================================================================================
+
+class max_heap:
+    def __init__(self, size:int) -> None:
+        pass
+
+    def parent(self, index:int) -> int:
+        return (index-1) // 2
+    
+    def left_child(self, index:int) -> int:
+        
+
+
+def main() -> None:
+    pass
+
+if __name__ == '__main__':
+    main()
